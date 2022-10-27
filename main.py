cc2bd5ebdc58718631c02b1f731a1504eff26c13
lenkaB-0101 <96007039+lenkaB-0101@users.noreply.github.com>
2022-10-27 13:40:56 +0200

    add

--- main.py
+++ main.py
@@ -3,42 +3,41 @@ from tkinter import *
 # Okno
 window = Tk()
 window.title("Currency conversion")
-window.minsize(width=500, height=500)
+window.minsize(width=300, height=300)
 window.resizable(False, False)
 window.iconbitmap("ICON.ico")
 window.config(background="#f621d2")
 
-#window2 = tkinter.Tk()
-#window2.title("Second window")
-#window2.geometry("300x400+800+100")
-#window2.resizable(False, False)
-#window2.iconbitmap("ICON.ico")
-#window2.config(bg="#20ff0d")
+# 1 EUR = 24,58 Kč
+
+def count_currency():
+    amount_eur = float(amount_input.get()) / 24.58
+    result_l["text"] = round(amount_eur, 2)
+
+# vstup od uživatele
+amount_input = Entry(width=10, font=("Helvetica", 15))
+amount_input.grid(row=0, column=0, padx=10, pady=10)
 
 
 # Label
-currency = Label(text="EUR", bg="#f621d2", fg="black", font=("Helvetica", 24, "bold"), borderwidth=5, relief="sunken")
-currency.pack(ipadx=20)
+czk_label = Label(text="CZK", bg="#f621d2", fg="black", font=("Helvetica", 15), borderwidth=5, relief="sunken")
+czk_label.grid(row=0, column=1)
 
-#currency2 = Label(window, text="CZK", font=("Cambira", 24, "bold"), bg="#f621d2", fg="black", borderwidth=5, relief="sunken")
-#currency2.pack(ipadx=20)
+result_l = Label(text="0", font=("Helvetica", 15), bg="#f621d2", fg="black")
+result_l.grid(row=1, column=0)
 
+eur_l= Label(text="EUR", font=("Helvetica", 15), bg="#f621d2", fg="black", borderwidth=5, relief="sunken")
+eur_l.grid(row=1, column=1)
 
-#změna textu
-#currency["text"] = "New"
-#currency.config(text="New2")
 
 # tlačítko
-def change_text():
-    currency["text"] = input_1.get()
+count_button = Button(text="Conversion ", font=("Helvetica", 15), command=count_currency)
+count_button.grid(row=0, column=2, padx=10, pady=10)
 
 
-button_1 = Button(text="Click", command=change_text)
-button_1.pack()
 
-# Input
-input_1 = Entry(width=20)
-input_1.pack()
+
+
 
 
 # Hlavní cyklus
